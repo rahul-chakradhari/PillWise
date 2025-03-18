b46958cf2e22ff421c0bc98cf25a31fe0661bf38
Vika1402 <vikas.kumar123@ssipmt.com>
2025-03-19 00:05:21 +0530

    Enhance Navbar styling by adding padding to the brand link and removing unused button code

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -40,15 +40,10 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand p-3" to="/">
           MediTrack
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-        ></button>
+       
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
